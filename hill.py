2640f1ec87134eda03fe198e6313a54c73ceef7a
Haruka Shirayuki <arung.agamani@gmail.com>
2020-09-05 17:11:44 +0800

    init hill.py

---
+++ hill.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+class Hill():
+    def create_matrix(self, mat):
+        pass
+
+    def verify_matrix(self, mat):
+        pass
+
+    def encrypt(self):
+        pass
+
+    def decrypt(self):
+        pass
