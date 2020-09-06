75b78613117d7eb124dcc65123f0c56bd6f58e06
Haruka Shirayuki <arung.agamani@gmail.com>
2020-09-06 19:36:30 +0800

    Fixed static files serving

--- app/static/scripts/index.js
+++ app/static/scripts/index.js
@@ -2,4 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import HelloWorld from "./App";
   
-ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<HelloWorld />, document.getElementById("app"));
